d425a8c2246dc20006894f091e207dca80b21cb2
Fer <fernanda.marana44@gmail.com>
2018-04-26 16:45:37 -0300

    Fiz um jeito de salvar e tentei aplica-lo nas cenas para testa-lo, mas ainda n consegui. Salvando agora para tentar testar outro método de salvar

---
+++ Assets/Scripts/DataToSave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataToSave : MonoBehaviour {
+	//public static int Dinheiro;
+	private int Number_Level = 1 ;
+	public GameObject[] Datas;
+	public static int[] Scores;
+
+	void Awake(){
+		Datas = GameObject.FindGameObjectsWithTag ("DATA");
+		if (Datas.Length >= 2) {
+			Destroy (Datas [0]);
+		}
+		DontDestroyOnLoad (transform.gameObject);
+	}
+	void Start(){
+		for (int x = 0; x < Number_Level; x++) {
+			PlayerPrefs.SetInt("Scores"+ x, Scores[x]);
+				}
+		for (int x = 0; x < Number_Level; x++) {
+			Scores[x] =  PlayerPrefs.GetInt("Scores"+ x);
+		}
+				
+		}
+
+	}
+	
