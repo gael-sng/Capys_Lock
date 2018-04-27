f0b54bd7a8ae1163bf0701d727545d0b25313732
Fer <fernanda.marana44@gmail.com>
2018-04-27 15:36:39 -0300

    User agora consegue editar o volume do audio e ainda é salvo para quando ele fecha e volta ao jogo

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour {
 
 	public AudioMixer audioMixer;
 	public GameObject objectWithScript;
 	private int volume;
+	private GameObject sliderVolume;
 
 	public void Start(){
+		sliderVolume = GameObject.Find ("SliderVolume");
 		volume = objectWithScript.GetComponent<DataToSave> ().LoadVolume () ;
-
+		sliderVolume.GetComponent<Slider>().value = volume;
 	}
 
-	public void SetVolume(float volume){
-		audioMixer.SetFloat ("Volume", volume);
+	public void SetVolume(){
+		
+		audioMixer.SetFloat ("Volume", sliderVolume.GetComponent<Slider>().value );
+		objectWithScript.GetComponent<DataToSave> ().SaveVolume ((int) sliderVolume.GetComponent<Slider>().value );
+
+	
 	}
 
 
